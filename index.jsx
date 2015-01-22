4ecd5e86c3007a540fb17b973f25883e9ff208b3
flemdizzle <fleminsh@gmail.com>
2015-01-22 14:24:02 -0500

    got dynamically loading comments working with react

--- index.jsx
+++ index.jsx
@@ -4,3 +4,85 @@ var contentDiv = document.createElement("DIV");
 contentDiv.id = "content";
 document.body.appendChild(contentDiv);
 
+// var converter = new Showdown.converter();
+
+var data = [
+  {author: "Pete Hunt", text: "this is one comment"},
+  {author: "Jordan Walker", text: "this is another comment"}
+];
+
+var Comment = React.createClass({
+  render: function(){
+    return (
+      <div className="comment">
+        <h2 className="commentAuthor">
+          {this.props.author}
+        </h2>
+        {this.props.children}
+      </div>
+    );
+  }
+})
+
+var CommentList = React.createClass({
+  render: function(){
+    var commentNodes = this.props.data.map(function(comment){
+      return(
+        <Comment author={comment.author}>
+          {comment.text}
+        </Comment>
+      );
+    });
+    return(
+      <div className="commentList">
+        {commentNodes}
+      </div>
+    );
+  }
+})
+
+var CommentForm = React.createClass({
+  render: function(){
+    return(
+      <div className="commentForm">
+        Form
+      </div>
+    );
+  }
+})
+
+var CommentBox = React.createClass({
+  loadCommentsFromServer: function() {
+    $.ajax({
+      url: this.props.url,
+      dataType: 'json',
+      success: function(data) {
+        this.setState({data: data});
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
+  },
+  getInitialState: function() {
+    return {data: []};
+  },
+  componentDidMount: function() {
+    this.loadCommentsFromServer();
+    setInterval(this.loadCommentsFromServer, this.props.pollInterval);
+  },
+  render: function() {
+    return (
+      <div className="commentBox">
+        <h1>Comments</h1>
+        <CommentList data={this.state.data} />
+        <CommentForm />
+      </div>
+    );
+  }
+});
+
+React.render(
+  <CommentBox url="comments.json" pollInterval={2000} />,
+  document.getElementById('content')
+);
