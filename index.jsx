394b660a7befdbf6ea55727c614e43d56497965c
flemdizzle <fleminsh@gmail.com>
2015-01-22 11:50:40 -0500

    apparently DOMContentLoaded doesn't work with JSX

--- index.jsx
+++ index.jsx
@@ -1,8 +1,6 @@
-document.addEventListener("DOMContenLoaded", function(event){
 
 //adds the content div
 var contentDiv = document.createElement("DIV");
 contentDiv.id = "content";
 document.body.appendChild(contentDiv);
 
-});
